d2e07bf94212a8b25fc0dff5e2823266f447fef8
Susana Alba Najera <1fsusanaalbanajera@gmail.com>
2022-06-14 12:33:03 -0500

    Style: Changes of style were added

--- src/App.jsx
+++ src/App.jsx
@@ -15,48 +15,98 @@ function App() {
         </button>
     )
   })
+  const [people, setPeople] = useState([
+    {
+      name: "Susana",
+      lastName: "Alba",
+      age: 20,
+      favouriteFood: "Hamburgers",
+      favouriteColour: "Pink",
+      counter: 0,
+    },
+  ]);
+  const [name, setName] = useState("");
+  const [lastName, setLastname] = useState("");
+
   return (
-    <div className="wrapper">
-      <div className="show-input">
-        Resultado : {data}
+    <div className="App">
+      <div className="wrapper">
+        <div className="show-input">Resultado : {data}</div>
+        <div className="digits flex">{calcBtns}</div>
+        <div className="modifiers subgrid">
+          <button onClick={() => setData(data.substr(0, data.length - 1))}>
+            Clear
+          </button>
+          <button onClick={() => setData("")}>AC</button>
+        </div>
+        <div className="operations subgrid">
+          <button onClick={(e) => setData(data + e.target.value)} value="+">
+            +
+          </button>
+          <button onClick={(e) => setData(data + e.target.value)} value="-">
+            -
+          </button>
+          <button onClick={(e) => setData(data + e.target.value)} value="*">
+            *
+          </button>
+          <button onClick={(e) => setData(data + e.target.value)} value="/">
+            /
+          </button>
+          <button
+            onClick={(e) => {
+              try {
+                setData(
+                  String(eval(data)).length > 3 &&
+                    String(eval(data)).includes(".")
+                    ? String(eval(data).toFixed(4))
+                    : String(eval(data))
+                );
+              } catch (err) {
+                console.log(err);
+              }
+            }}
+            value="="
+          >
+            =
+          </button>
+        </div>
       </div>
-      <div className="digits flex">{calcBtns}</div>
-      <div className="modifiers subgrid">
-        <button onClick={() => setData(data.substr(0,data.length - 1))}>
-          Clear
-        </button>
-        <button onClick={() => setData("")}>
-          AC
-        </button>
-      </div>
-      <div className="operations subgrid">
-        <button onClick={e => setData(data + e.target.value)} value = "+">
-          +
-        </button>
-        <button onClick={e => setData(data + e.target.value)} value = "-">
-          -
-        </button>
-        <button onClick={e => setData(data + e.target.value)} value = "*">
-          *
-        </button>
-        <button onClick={e => setData(data + e.target.value)} value = "/">
-          /
-        </button>
-        <button onClick={e =>{
-          try {
-            setData(
-              String(eval(data)).length > 3 &&
-              String(eval(data)).includes(".")
-              ? String(eval(data).toFixed(4))
-              : String(eval(data))
-            )
-          }
-          catch(err){
-            console.log(err);
-          }
-        }}value = "=">
-          =
-        </button>
+      <div className="users">
+        <div className="inputs">
+          <input
+            className="input"
+            placeholder="Nombre"
+            type="text"
+            name="name"
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+          <input
+            className="input"
+            placeholder="Apellido"
+            type="text"
+            name="lastName"
+            onChange={(event) => {
+              setLastname(event.target.value);
+            }}
+          />
+          <button
+            className="button"
+            onClick={() => {
+              setPeople((current) => [{ name, lastName }, ...current]);
+            }}
+          >
+            Add user
+          </button>
+        </div>
+        <div className="list">
+          <ul>
+            {people.map((person, idx) => (
+              <li id={idx}>{`${person.name} ${person.lastName}`}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
